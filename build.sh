b23e8d2db520954573db52d3ecc7a7793fccf5f8
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-06-25 13:21:49 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,5 +18,7 @@ git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi; 
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+
 . build/envsetup.sh; \
-brunch munch
+lunch horizon_munch-bp1a-userdebug
+mka horizon
