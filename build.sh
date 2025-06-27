5e25b264ed1bae646f85b3e711688b9c8ca0a6e5
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-06-27 19:28:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,6 @@ rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && r
 git clone https://github.com/Lordputin404/android_device_xiaomi_munch device/xiaomi/munch; \
 git clone https://github.com/Lordputin404/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch vendor/xiaomi/munch; \
-git clone https://gitlab.com/rik-x777/keys vendor/lineage-priv/keys; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 git clone https://gitlab.com/rik-x777/keys.git vendor/lineage-priv/keys; \
