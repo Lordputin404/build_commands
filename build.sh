3b17a1187fe473b43b0572bd68daed0b6d34c0cd
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-06-28 06:36:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,22 +7,23 @@ repo init -u https://github.com/HorizonV2/android -b lineage-22.2 --git-lfs; \
 rm -rf out/target/product/munch && rm -rf device/xiaomi/munch && rm -rf device/xiaomi/sm8250-common && rm -rf kernel/xiaomi/sm8250; \
 rm -rf vendor/xiaomi/munch && rm -rf vendor/xiaomi/sm8250-common && rm -rf hardware/xiaomi && rm -rf vendor/xiaomi/munch-firmware; \
 rm -rf vendor/xiaomi/miuicamera && rm -rf packages/resources/devicesettings && rm -rf packages/apps/ViPER4AndroidFX; \
+rm -rf vendor/lineage-priv/keys; \
 
 git clone https://github.com/Lordputin404/android_device_xiaomi_munch device/xiaomi/munch; \
 git clone https://github.com/Lordputin404/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch vendor/xiaomi/munch; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
+git clone https://gitlab.com/rik-x777/keys vendor/lineage-priv/keys; \
 git clone https://gitlab.com/rik-x777/kernel_xiaomi_sm8250 kernel/xiaomi/sm8250; \
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
-rm -rf device/lineage/sepolicy; \
-git clone https://github.com/rik-x777/android_device_lineage_sepolicy -b qpr2 device/lineage/sepolicy; \
+#rm -rf device/lineage/sepolicy; \
+#git clone https://github.com/rik-x777/android_device_lineage_sepolicy -b qpr2 device/lineage/sepolicy; \
 
-rm -rf vendor/lineage-priv/keys; \
 git clone https://gitlab.com/rik-x777/keys.git vendor/lineage-priv/keys; \
 
 . build/envsetup.sh; \
