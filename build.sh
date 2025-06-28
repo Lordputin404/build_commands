8519270bc06896be704542e66e2a978fb4d566bb
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-06-28 15:28:07 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -29,10 +29,6 @@ git clone https://github.com/munch-devs/android_vendor_xiaomi_munch vendor/xiaom
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 #git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
-#keys
-git clone https://github.com/munch-devs/private-keys vendor/lineage-priv/keys; \
-#git clone https://gitlab.com/rik-x777/keys vendor/lineage-priv/keys; \
-
 #kernel
 git clone https://github.com/munch-devs/kernel_xiaomi_munch kernel/xiaomi/sm8250; \
 cd kernel/xiaomi/sm8250 && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
@@ -46,6 +42,10 @@ git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+
+#keys
+git clone https://github.com/munch-devs/private-keys vendor/lineage-priv/keys; \
+#git clone https://gitlab.com/rik-x777/keys vendor/lineage-priv/keys; \
 
 #rm -rf device/lineage/sepolicy; \
 #git clone https://github.com/rik-x777/android_device_lineage_sepolicy -b qpr2 device/lineage/sepolicy; \
