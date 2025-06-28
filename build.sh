b2424e2502113a81165fd196a7df2fc61af4e612
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-06-28 12:00:45 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,12 +11,12 @@ rm -rf vendor/lineage-priv/keys; \
 
 #Own 
 #git clone https://github.com/Lordputin404/android_device_xiaomi_munch device/xiaomi/munch; \
-git clone https://github.com/munch-devs/android_device_xiaomi_munch device/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx device/xiaomi/munch; \
 
 git clone https://github.com/Lordputin404/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
 
 #vendor
-https://github.com/munch-devs/android_vendor_xiaomi_munch vendor/xiaomi/munch; \
+git clone https://github.com/munch-devs/android_vendor_xiaomi_munch vendor/xiaomi/munch; \
 #git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch vendor/xiaomi/munch; \
 git clone https://gitlab.com/rik-x777/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
 
@@ -29,7 +29,8 @@ git clone https://github.com/munch-devs/private-keys vendor/lineage-priv/keys; \
 #git clone https://gitlab.com/rik-x777/keys vendor/lineage-priv/keys; \
 
 #kernel
-git clone https://gitlab.com/rik-x777/kernel_xiaomi_sm8250 kernel/xiaomi/sm8250; \
+git clone https://github.com/munch-devs/kernel_xiaomi_munch kernel/xiaomi/sm8250; \
+#git clone https://gitlab.com/rik-x777/kernel_xiaomi_sm8250 kernel/xiaomi/sm8250; \
 
 #hardware
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
