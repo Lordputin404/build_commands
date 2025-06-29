8a0c746fc2980a7dfabcb8cd7b25a3826efb3e19
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-06-29 07:10:18 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ git clone https://github.com/Evolution-X-Devices/hardware_xiaomi hardware/xiaomi
 #git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
 
 #additonals
-git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
+git clone https://github.com/Lordputin404/android_hardware_dolby hardware/dolby; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
