e6956026a45d4552a97611d303b65e2e02d8d581
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-06-29 13:21:46 +0530

    Refreshing clones

--- build.sh
+++ build.sh
@@ -13,13 +13,16 @@ rm -rf hardware/dolby; \
 rm -rf vendor/xiaomi/miuicamera; \
 rm -rf packages/resources/devicesettings; \
 
-git clone https://github.com/efeisot/android_device_xiaomi_munch device/xiaomi/munch; \
-git clone https://github.com/efeisot/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common; \
-git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch vendor/xiaomi/munch; \
-git clone https://gitlab.com/rik-x777/vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common; \
-git clone https://gitlab.com/rik-x777/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
-git clone https://gitlab.com/rik-x777/kernel_xiaomi_sm8250 kernel/xiaomi/sm8250; \
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi; \
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b clover device/xiaomi/munch; \
+
+git clone https://github.com/munch-devs/android_vendor_xiaomi_munch vendor/xiaomi/munch; \
+
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
+
+git clone https://github.com/munch-devs/kernel_xiaomi_munch -b munch-ksu kernel/xiaomi/sm8250; \
+
+git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
+
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
