40a00efc9c6ef0b98392670ba04412bcea370f04
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-06-29 21:44:19 +0530

    Dolby hardware add

--- build.sh
+++ build.sh
@@ -23,6 +23,8 @@ git clone https://github.com/munch-devs/kernel_xiaomi_munch -b munch-ksu kernel/
 
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
 
+git clone https://github.com/munch-devs/android_hardware_dolby; \
+
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
