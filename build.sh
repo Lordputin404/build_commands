ac2e791d87b6e8982426cd6e36e9585cfe25f208
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-06-29 13:27:01 +0530

    Final finish

--- build.sh
+++ build.sh
@@ -24,7 +24,13 @@ git clone https://github.com/munch-devs/kernel_xiaomi_munch -b munch-ksu kernel/
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
 
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
+
 git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
+
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+
+
 . build/envsetup.sh; \
-brunch munch
+
+lunch clover_munch-bp1a-userdebug;/
+mka clover;/
