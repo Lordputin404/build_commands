174d8aaa8e94ef9ed5398c4e0fe95c3f307f480a
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-07-01 21:14:11 +0530

    Final Yaap

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/CherishOS/android_manifest.git -b sixteen --git-lfs; \
+repo init -u https://github.com/yaap/manifest -b sixteen --git-lfs; \
 /opt/crave/resync.sh; \
 
 rm -rf out/target/product/munch; \
@@ -13,23 +13,23 @@ rm -rf hardware/xiaomi; \
 rm -rf hardware/dolby; \
 rm -rf vendor/xiaomi/miuicamera; \
 rm -rf packages/resources/devicesettings; \
-rm -rf device/cherish/sepolicy; \
+#rm -rf device/yaap/sepolicy; \
 rm -rf packages/apps/ViPER4AndroidFX; \
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b cherish device/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b yaap device/xiaomi/munch; \
 
 git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16 vendor/xiaomi/munch; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
-git clone https://github.com/munch-devs/kernel_xiaomi_munch -b 16-ksu kernel/xiaomi/munch; \
-cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b 16 kernel/xiaomi/sm8250; \
+cd kernel/xiaomi/sm8250 && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
 
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
 
 git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
 
-git clone https://github.com/rik-x777/android_device_lineage_sepolicy -b qpr2 device/cherish/sepolicy; \
+#git clone https://github.com/rik-x777/android_device_lineage_sepolicy -b qpr2 device/cherish/sepolicy; \
 
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 
@@ -40,4 +40,4 @@ git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendo
 
 . build/envsetup.sh; \
 
-brunch munch
+lunch yaap_munch-userdebug && m yaap
