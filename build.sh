1d0d57906c0a07953d348e19197cc66788dd0b97
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-07-06 17:15:43 +0530

    Rm rf command for fixing trusty errors

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 rm -rf .repo/local_manifests; \
 repo init -u https://github.com/VoltageOS-staging/manifest -b 16 --git-lfs; \
+rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
 rm -rf out/target/product/munch; \
