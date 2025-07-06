e72b77093dc5e64c5758018178283bd7feb0f68e
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-07-06 17:05:05 +0530

    Updated vendor branch to 16

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ rm -rf packages/apps/ViPER4AndroidFX; \
 
 git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b clover device/xiaomi/munch; \
 
-git clone https://github.com/munch-devs/android_vendor_xiaomi_munch vendor/xiaomi/munch; \
+git clone https://github.com/munch-devs/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
