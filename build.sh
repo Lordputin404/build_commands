62b4e4ede25db0f4c6b40e786ed6865bb351f2dd
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-07-10 22:30:52 +0530

    Changed dt and hardware source

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ rm -rf packages/resources/devicesettings; \
 rm -rf packages/apps/ViPER4AndroidFX; \
 
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b voltage device/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b aqua device/xiaomi/munch; \
 
 git clone https://github.com/munch-devs/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
 
@@ -27,13 +27,13 @@ git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware v
 git clone https://github.com/Lordputin404/fusionX_sm8250 -b a16 kernel/xiaomi/munch; \
 cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
 
-git clone https://github.com/VoltageOS-staging/hardware_xiaomi hardware/xiaomi; \
+git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
 
 git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
 
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 
-git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
+#git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
