1795c9f45cbd6bbe5d7fef841ba60e780d0767ba
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-07-12 11:03:45 +0530

    Mi cam commented

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ git clone https://github.com/PocoF3Releases/packages_resources_devicesettings pa
 
 #git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
 
 . build/envsetup.sh; \
