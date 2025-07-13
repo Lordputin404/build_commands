68b0adc8b60584a42fa59a5ca002bd7848ccd260
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-07-13 20:37:07 +0530

    Dropped viper4fx

--- build.sh
+++ build.sh
@@ -31,7 +31,7 @@ git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
 
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 
-git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
+#git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
 
