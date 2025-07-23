f08b17ef4a2061373cb080f9b8e61e2cecd701c3
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-07-23 23:39:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,43 +5,43 @@ repo init -u https://github.com/yaap/manifest -b sixteen --git-lfs; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
-rm -rf out/target/product/munch; \
-rm -rf device/xiaomi/munch; \
-rm -rf kernel/xiaomi/sm8250; \
-rm -rf vendor/xiaomi/munch-firmware; \
-rm -rf vendor/xiaomi/munch; \
-rm -rf hardware/xiaomi; \
-rm -rf hardware/dolby; \
-rm -rf vendor/xiaomi/miuicamera; \
-rm -rf packages/resources/devicesettings; \
-rm -rf packages/apps/FastCharge; \
-rm -rf packages/apps/ViPER4AndroidFX; \
+rm -rf out/target/product/munch
+rm -rf device/xiaomi/munch
+rm -rf kernel/xiaomi
+rm -rf vendor/xiaomi/munch-firmware
+rm -rf vendor/xiaomi/munch
+rm -rf hardware/xiaomi
+rm -rf hardware/dolby
+rm -rf vendor/xiaomi/miuicamera
+rm -rf packages/resources/devicesettings
+rm -rf packages/apps/FastCharge
+rm -rf packages/apps/ViPER4AndroidFX
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b yaap device/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b yaap device/xiaomi/munch
 
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16 vendor/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16 vendor/xiaomi/munch
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
-git clone https://github.com/Lordputin404/kernel_xiaomi_munch -b 16-ksu kernel/xiaomi/sm8250; \
-cd kernel/xiaomi/sm8250 && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
+git clone https://github.com/Lordputin404/kernel_xiaomi_munch -b 16-ksu kernel/xiaomi/munch
+cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
 
-git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
+git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi
 #git clone https://github.com/yaap/hardware_xiaomi hardware/xiaomi; \
 
-git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
+git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
 
 #git clone https://github.com/rik-x777/android_device_lineage_sepolicy -b qpr2 device/yaap/sepolicy; \
 
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
 
-git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
+git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
-git clone https://github.com/cyberknight777/android_packages_apps_FastCharge packages/apps/FastCharge; \
+git clone https://github.com/cyberknight777/android_packages_apps_FastCharge packages/apps/FastCharge
 
 
-source build/envsetup.sh; \
+source build/envsetup.sh
 
 lunch yaap_munch-userdebug && m yaap
