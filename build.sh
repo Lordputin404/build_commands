6f29a4c36b7c989bb582ffc672973cef730d727e
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-09-03 23:13:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ rm -rf packages/apps/ViPER4AndroidFX; \
 
 git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b mist device/xiaomi/munch; \
 
-git clone https://github.com/munch-devs/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
+git clone https://github.com/munch-devs/android_vendor_xiaomi_munch -b 16-exp vendor/xiaomi/munch; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
