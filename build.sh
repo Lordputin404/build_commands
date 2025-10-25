80b1a577e77e3b68fe632620f56bc4485b7877a1
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-10-25 08:38:04 +0530

    Update kernel repository to fusionX_sm8250

--- build.sh
+++ build.sh
@@ -25,7 +25,8 @@ git clone https://github.com/Olzhas-Kdyr/proprietary_vendor_xiaomi_sm8250-common
 git clone https://github.com/PocoF4Trees/vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
 # Kernel Tree
-git clone --depth=1 https://github.com/PocoF4Trees/kernel_xiaomi_sm8250 -b staging-noksu kernel/xiaomi/sm8250
+git clone --depth=1 https://github.com/SenseiiX/fusionX_sm8250 -b stable-next kernel/xiaomi/sm8250
+cd kernel/xiaomi/sm8250 && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
 
 # Hardware Xiaomi
 git clone https://github.com/Project-SenX/android_hardware_xiaomi hardware/xiaomi
