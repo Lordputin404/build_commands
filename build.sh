4396357e4f493444abe248570f3fe849f8fe3e9e
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-11-01 17:03:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
 #git clone https://github.com/Lordputin404/kernel_xiaomi_munch -b 16-ksu kernel/xiaomi/munch
-git clone https://github.com/munch-devs/kernel_xiaomi_munch -b new-ksu kernel/xiaomi/munch
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b stable-next kernel/xiaomi/munch
 cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
 
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi
@@ -41,4 +41,4 @@ git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendo
 
 source build/envsetup.sh
 lunch lumine_munch-bp2a-user
-mka bacon -j16
+mka bacon 
