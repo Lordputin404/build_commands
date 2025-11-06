d68ac113d7b2356b3649dde1716f4c27bc0519da
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-11-07 00:54:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,43 +1,42 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-rm -rf packages/resources/devicesettings; \
-repo init -u https://github.com/ProjectInfinity-X/manifest -b 16 --git-lfs; \
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
-rm -rf out/target/product/munch; \
-rm -rf device/xiaomi/munch; \
-rm -rf kernel/xiaomi/munch; \
-rm -rf vendor/xiaomi/munch-firmware; \
-rm -rf vendor/xiaomi/munch; \
-rm -rf hardware/xiaomi; \
-rm -rf hardware/dolby; \
-rm -rf vendor/xiaomi/miuicamera; \
-rm -rf packages/resources/devicesettings; \
-rm -rf packages/apps/ViPER4AndroidFX; \
+mf=(
+device/xiaomi/munch
+kernel/xiaomi/munch
+vendor/xiaomi/munch
+vendor/xiaomi/munch-firmware
+hardware/xiaomi
+hardware/dolby
+packages/resources/devicesettings
+vendor/xiaomi/miuicamera
+)
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b infinity device/xiaomi/munch; \
+rm -rf "${mf[@]}"
 
-git clone https://github.com/munch-devs/android_vendor_xiaomi_munch -b 16-exp vendor/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b infinity device/xiaomi/munch
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
+git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16-exp vendor/xiaomi/munch
 
-git clone https://github.com/munch-devs/kernel_xiaomi_munch -b munch-ksu kernel/xiaomi/munch; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
-git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
+git clone https://github.com/munch-devs/kernel_xiaomi_munch -b munch-ksu kernel/xiaomi/munch
 
-git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
+git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi
 
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
+git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
 
-#git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+#git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
-git clone https://github.com/munch-devs/android_vendor_bcr -b vic vendor/bcr
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
-. build/envsetup.sh; \
-lunch infinity_munch-bp2a-user
-make installclean
+
+source build/envsetup.sh
+lunch infinity_munch-user
 m bacon
