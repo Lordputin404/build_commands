9739e8cf02267ae0a1d0975db23b8153089c7f40
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-11-07 03:18:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-rm -rf .repo/local_manifests
+rm -rf .repo
 repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 rm -rf prebuilts/clang/host/linux-x86
 /opt/crave/resync.sh
