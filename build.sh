a246e206347cc9879a7547ab82c842a934216a21
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2025-11-08 16:23:50 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,20 +15,16 @@ packages/apps/XiaomiDolby
 
 rm -rf "${mf[@]}"
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b infinity device/xiaomi/munch
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16-exp vendor/xiaomi/munch
+git clone https://github.com/Ronin-293/android_device_xiaomi_onyx -b lineage-23.0 device/xiaomi/onyx
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
+git clone https://github.com/Ronin-293/proprietary_vendor_xiaomi_onyx -b lineage-23.0 vendor/xiaomi/onyx
 
-git clone https://github.com/Ronin-293/android_device_xiaomi_onyx-kernel -b 16-ksu kernel/xiaomi/onyx
-cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..
+git clone https://github.com/Ronin-293/android_device_xiaomi_onyx-kernel -b lineage-23.0 kernel/xiaomi/onyx
 
 git clone https://github.com/Ronin-293/android_hardware_xiaomi hardware/xiaomi
 
 
-
-
 source build/envsetup.sh
-lunch infinity_munch-user
+lunch clover_onyx-bp2a-user
 make installclean
-m bacon
+mka clover
