84b14c2ee6be367c6847e38c07c7c43cc422b542
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-01-03 16:54:54 +0530

    Update Dolby hardware repository URL in build script

--- build.sh
+++ build.sh
@@ -29,7 +29,9 @@ cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf K
 
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
 
-git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
+git clone https://github.com/Pong-Development/hardware_dolby; \
+
+#git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
 
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 
