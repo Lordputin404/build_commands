17a8c6124a409f96ac39234b6f2b8c6d9e43e9f9
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-01-10 15:45:38 +0530

    Update build script for new repositories and branches

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs; \
+repo init -u https://github.com/AOSPA/manifest -b vauxite --git-lfs; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
@@ -18,27 +18,22 @@ vendor/xiaomi/miuicamera
 
 rm -rf "${mf[@]}"
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b evox device/xiaomi/munch
+git clone https://github.com/Ronin-293/android_device_xiaomi_munch -b vauxite device/xiaomi/munch
 
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16-exp vendor/xiaomi/munch
+git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 15-qpr2 vendor/xiaomi/munch
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
+#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
-#git clone https://github.com/Lordputin404/kernel_xiaomi_munch -b 16-ksu kernel/xiaomi/munch
-git clone https://github.com/munch-devs/kernel_xiaomi_munch -b new-ksu kernel/xiaomi/munch
-cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b stable-noksu kernel/xiaomi/munch
 
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi
 
-git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
+#git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
 
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
 
 #git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
-
-source build/envsetup.sh
-lunch lineage_munch-bp2a-user
-m evolution
+./rom-build.sh munch
