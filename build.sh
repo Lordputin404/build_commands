b169e3c6579aebb67e1cac8663f635cc1398fbfc
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-01-25 09:14:24 +0530

    Modify build script for repository setup
    Updated repository initialization and commented out multiple git clone commands for various components.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests/
-repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr1 --git-lfs
+repo init -u https://github.com/AOSPA/manifest -b uvite --git-lfs
 /opt/crave/resync.sh
 
 mf=(
@@ -18,22 +18,20 @@ vendor/xiaomi/miuicamera
 
 rm -rf "${mf[@]}"
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b pixel device/xiaomi/munch
+#git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b pixel device/xiaomi/munch
 
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16-exp vendor/xiaomi/munch
+#git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16-exp vendor/xiaomi/munch
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
+#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
-git clone https://github.com/SenseiiX/fusionX_sm8250 -b bkp kernel/xiaomi/munch
+#git clone https://github.com/SenseiiX/fusionX_sm8250 -b bkp kernel/xiaomi/munch
 
-git clone https://github.com/Kenskuyy-Prjkt/hardware_xiaomi hardware/xiaomi
+#git clone https://github.com/Kenskuyy-Prjkt/hardware_xiaomi hardware/xiaomi
 
-git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
+#git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
 
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
+#git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+#git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
-source build/envsetup.sh
-breakfast munch
-m pixelos
+./rom-build.sh munch
