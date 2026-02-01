9db253e56e8cf024a347970406632a4f1e10aebb
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-02-01 07:46:34 +0530

    Update build script for new repository sources

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs; \
+repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
@@ -12,26 +12,24 @@ vendor/xiaomi/munch
 vendor/xiaomi/munch-firmware
 hardware/xiaomi
 hardware/dolby
-packages/resources/devicesettings
 vendor/xiaomi/miuicamera
 )
 
 rm -rf "${mf[@]}"
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b axion device/xiaomi/munch
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b yaap device/xiaomi/munch
 
 git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16 vendor/xiaomi/munch
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
-git clone https://github.com/SenseiiX/fusionX_sm8250 -b stable-susfs kernel/xiaomi/munch
-cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b bkp kernel/xiaomi/munch
 
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi
 
 git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
 
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
+#git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
 
 #git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
@@ -39,6 +37,4 @@ git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendo
 
 
 source build/envsetup.sh
-gk -s
-axion munch userdebug va
-ax -br -j16
+lunch yaap_munch-user && m yaap
