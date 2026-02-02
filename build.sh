9808bdb239e09754b4c0cf5242caccb5ff3801a8
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-02-02 08:01:50 +0530

    Update git clone commands for Xiaomi device repos

--- build.sh
+++ build.sh
@@ -17,9 +17,9 @@ vendor/xiaomi/miuicamera
 
 rm -rf "${mf[@]}"
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch_hdzungx -b yaap device/xiaomi/munch
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b yaap device/xiaomi/munch
 
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16 vendor/xiaomi/munch
+git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
