75154b743ebd1f83d13b1d70e24360502efa5920
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-02-08 10:24:04 +0530

    Update build script for new branch and cleanup

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs; \
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
-
-rm -rf out/target/product/munch; \
 rm -rf device/xiaomi/munch; \
 rm -rf kernel/xiaomi/munch; \
 rm -rf vendor/xiaomi/munch-firmware; \
@@ -20,8 +18,8 @@ rm -rf packages/apps/ViPER4AndroidFX; \
 
 git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b lunaris-test device/xiaomi/munch; \
 
-#git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16-exp vendor/xiaomi/munch; \
-git clone https://github.com/Kenskuyy-Prjkt/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
+
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
@@ -39,5 +37,5 @@ git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendo
 
 
 . build/envsetup.sh; \
-lunch lineage_munch-bp2a-user
+lunch lineage_munch-bp4a-user
 m lunaris
