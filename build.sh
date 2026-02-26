df6f7f5707243f7515232a92f38e6af4788516e2
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-02-26 09:02:41 +0530

    Change axion command from 'va' to 'gms pico'

--- build.sh
+++ build.sh
@@ -38,5 +38,5 @@ git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendo
 cd hard*/line*/compat/ &&git fetch https://github.com/LineageOS/android_hardware_lineage_compat refs/changes/04/447604/1 && git cherry-pick FETCH_HEAD && cd -
 source build/envsetup.sh
 gk -s
-axion munch va
+axion munch gms pico
 ax -br -j16
