c67d94b49542ec2b7dfd8d07822fb8d051ddb208
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-02-26 10:37:56 +0530

    Update branch version in build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.2 --git-lfs; \
+repo init -u repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.1 --git-lfs; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
