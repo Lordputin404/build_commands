a2199abf39ed1c77fb9fe10766fbe1bc27890622
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-03-01 10:36:33 +0530

    Update build script for new branch and repositories

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests/
-repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr1 --git-lfs
+repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b sixteen-qpr2 --git-lfs
 /opt/crave/resync.sh
 
 mf=(
@@ -18,9 +18,9 @@ vendor/xiaomi/miuicamera
 
 rm -rf "${mf[@]}"
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b pixel device/xiaomi/munch
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b pixelwip device/xiaomi/munch
 
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16-exp vendor/xiaomi/munch
+git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
@@ -30,7 +30,7 @@ git clone https://github.com/Kenskuyy-Prjkt/hardware_xiaomi hardware/xiaomi
 
 git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
 
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
+#git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
