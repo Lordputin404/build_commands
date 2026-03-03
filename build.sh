352a5022ec0c177289bc53c45f71628a1a50b9e2
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-03-03 17:16:52 +0530

    Replace kernel repository URL in build script

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16 vend
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
-git clone https://github.com/Olzhas-Kdyr/kernel_xiaomi_sm8250-oxy -b munch-23.2 kernel/xiaomi/munch; \
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b bkp kernel/xiaomi/munch
 
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
 
