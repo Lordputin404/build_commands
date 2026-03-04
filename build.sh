b0986748a68488221ab235599cf4eceb5cf91207
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-03-04 18:43:16 +0530

    Update build script for new branch and commands

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/AviumUI/android_manifests -b avium-16 --git-lfs; \
+repo init -u https://github.com/AviumUI/android_manifests -b avium-16.2 --git-lfs; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
@@ -20,11 +20,11 @@ rm -rf packages/apps/ViPER4AndroidFX; \
 
 git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b avium device/xiaomi/munch; \
 
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16-exp vendor/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16 vendor/xiaomi/munch; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
-git clone https://github.com/SenseiiX/fusionX_sm8250 -b stable-next kernel/xiaomi/munch; \
+git clone https://github.com/SenseiiX/fusionX_sm8250 -b bkp kernel/xiaomi/munch; \
 
 git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
 
@@ -36,6 +36,5 @@ git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendo
 
 
 . build/envsetup.sh; \
-avium get_gms
-lunch lineage_munch-bp2a-user
-m bacon
+lunch lineage_$device-bp4a-userdebug
+m bacon -j$(nproc --all)
