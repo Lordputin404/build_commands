68d68383d45bb03502124db1442ff14f169ea6b2
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-03-04 23:36:44 +0530

    Fix vendor repository clone URL in build.sh

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ rm -rf packages/apps/ViPER4AndroidFX; \
 
 git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b avium device/xiaomi/munch; \
 
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16 vendor/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
