7a595ca7d5df1f127ad982feaa9c960fc4155250
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-03-20 08:11:43 +0530

    Update repository initialization and cloning commands

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/Project-Mist-OS/manifest -b 4.3 --git-lfs; \
+repo init -u https://github.com/Project-Mist-OS/manifest.git -b 16.2 --git-lfs; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
@@ -19,7 +19,7 @@ rm -rf packages/apps/ViPER4AndroidFX; \
 
 git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b mist device/xiaomi/munch; \
 
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch_hdzungx -b 16-exp vendor/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
