bdb0988acf3926f748095ff9d013308a76f323bf
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-03-21 22:27:33 +0530

    Uncomment clone command for device settings package

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaom
 
 git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
 
-#git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
 
 #git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
