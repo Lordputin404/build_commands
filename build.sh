9399fb15d86eea2b199edc24099e5e4ddf3fb252
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-03-31 13:33:01 +0530

    Update repository initialization and clone commands

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
+repo init -u https://gitlab.e.foundation/e/os/android.git -b a16 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
@@ -35,7 +35,7 @@ git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/app
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
-cd hard*/line*/compat/ &&git fetch https://github.com/LineageOS/android_hardware_lineage_compat refs/changes/04/447604/1 && git cherry-pick FETCH_HEAD && cd -
+#cd hard*/line*/compat/ &&git fetch https://github.com/LineageOS/android_hardware_lineage_compat refs/changes/04/447604/1 && git cherry-pick FETCH_HEAD && cd -
 
 
 . build/envsetup.sh
