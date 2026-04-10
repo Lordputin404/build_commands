0926187f314ac56956d870df0a9e93a652baead4
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-04-10 20:16:49 +0530

    Change branch in git clone command for munch device

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ rm -rf packages/resources/devicesettings; \
 rm -rf packages/apps/ViPER4AndroidFX; \
 
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b lunaris-test device/xiaomi/munch
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b lunaris device/xiaomi/munch
 
 git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
 
