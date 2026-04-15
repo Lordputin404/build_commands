491f5d3d23862b96e9806790bc3a0c3ea82eeb24
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-04-15 20:02:28 +0530

    Refactor build.sh to streamline cleanup and cloning

--- build.sh
+++ build.sh
@@ -6,33 +6,36 @@ rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
 
-rm -rf device/xiaomi/munch; \
-rm -rf kernel/xiaomi/munch; \
-rm -rf vendor/xiaomi/munch-firmware; \
-rm -rf vendor/xiaomi/munch; \
-rm -rf hardware/xiaomi; \
-rm -rf hardware/dolby; \
-rm -rf vendor/xiaomi/miuicamera; \
-rm -rf packages/resources/devicesettings; \
-rm -rf packages/apps/ViPER4AndroidFX; \
+rm -rf device/xiaomi/munch
+rm -rf kernel/xiaomi/munch
+rm -rf vendor/xiaomi/munch-firmware
+rm -rf vendor/xiaomi/munch
+rm -rf hardware/xiaomi
+rm -rf hardware/dolby
+rm -rf vendor/xiaomi/miuicamera
+rm -rf packages/resources/devicesettings
+rm -rf packages/apps/ViPER4AndroidFX
 
 
-git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b baikal device/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_device_xiaomi_munch -b baikal device/xiaomi/munch
 
-git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch; \
+git clone https://github.com/Lordputin404/android_vendor_xiaomi_munch -b 16 vendor/xiaomi/munch
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware
 
-git clone https://github.com/SenseiiX/fusionX_sm8250 -b bkp kernel/xiaomi/munch
+git clone https://github.com/kvsnr113/xiaomi_sm8250_kernel_e404 -b bpf kernel/xiaomi/munch
+cd kernel/xiaomi/munch
+git submodule update --init --remote --merge
+cd ../../..
 
-git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi; \
+git clone https://github.com/Lordputin404/android_hardware_xiaomi hardware/xiaomi
 
-git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
+git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby
 
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 cd hard*/line*/compat/ &&git fetch https://github.com/LineageOS/android_hardware_lineage_compat refs/changes/04/447604/1 && git cherry-pick FETCH_HEAD && cd -
 
-. build/envsetup.sh; \
+. build/envsetup.sh
 brunch munch
