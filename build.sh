0670383d7ea7d2897bc3acfbe8d3e68980f8d36d
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-04-20 17:32:00 +0530

    Comment out cherry-pick command in build.sh
    Comment out the cherry-pick command for LineageOS hardware compatibility.

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ git clone https://github.com/PocoF3Releases/packages_resources_devicesettings pa
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
 
-cd hard*/line*/compat/ &&git fetch https://github.com/LineageOS/android_hardware_lineage_compat refs/changes/04/447604/1 && git cherry-pick FETCH_HEAD && cd -
+#cd hard*/line*/compat/ &&git fetch https://github.com/LineageOS/android_hardware_lineage_compat refs/changes/04/447604/1 && git cherry-pick FETCH_HEAD && cd -
 
 git clone https://github.com/kenway214/packages_apps_TouchServices.git -b lineage-23.2 packages/apps/TouchServices
 git clone https://github.com/kenway214/packages_apps_GameBar.git -b lineage-23.2 packages/apps/GameBar
