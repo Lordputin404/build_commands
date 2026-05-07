27ea1395ba8b338a83b04fc3bcb15ec73e38278b
Lordputin404 <93831599+Lordputin404@users.noreply.github.com>
2026-05-07 08:32:31 +0530

    Update build.sh to modify cherry-pick and add apps
    Comment out the cherry-pick command and add new git clone commands for additional apps.

--- build.sh
+++ build.sh
@@ -32,7 +32,12 @@ git clone https://github.com/munch-devs/android_hardware_dolby hardware/dolby; \
 git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera; \
-cd hard*/line*/compat/ &&git fetch https://github.com/LineageOS/android_hardware_lineage_compat refs/changes/04/447604/1 && git cherry-pick FETCH_HEAD && cd -
+#cd hard*/line*/compat/ &&git fetch https://github.com/LineageOS/android_hardware_lineage_compat refs/changes/04/447604/1 && git cherry-pick FETCH_HEAD && cd -
+git clone https://github.com/kenway214/packages_apps_TouchServices.git -b lineage-23.2 packages/apps/TouchServices
+
+git clone https://github.com/Lordputin404/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
+
+git clone https://github.com/kenway214/packages_apps_GameBar.git packages/apps/GameBar
 
 . build/envsetup.sh; \
 mistify munch user
